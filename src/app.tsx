cee04682631e03ae78101291e23420ab2f1a5983
machinemessiah <mark.edward.franklin@gmail.com>
2024-07-17 06:34:57 -0400

    Tiny change to the CSS for a quick fix (#1)
    * Tiny change to the CSS for a quick fix
    
    Simply making sure the target of the CSS rules are always list-items found in the nav and not just any list-items (such as those identical list-items used in the panel queue).
    
    * Tiny CSS change to fix panel queue issue
    
    * Made the open/closed indicator a pseudo element
    Added a hover state, and support for nested folders (up to 10 levels deep)
    Moved all the styles to /src/styles/app.scss
    Imported the styles into the app.tsx file
    Deleted the extra app.tsx which was in the root.
    Added way too many comments, but reabability is king imo.
    
    * Cleanup - removed old compiled js file from the root.

--- src/app.tsx
+++ src/app.tsx
@@ -1,62 +1,9 @@
+// @note - everything we need is in this file
+import "./styles/app.scss";
 async function main() {
 	while (!Spicetify?.showNotification) {
 		await new Promise(resolve => setTimeout(resolve, 100));
 	}
-  
-	// Create a <style> element
-	var style = document.createElement('style');
-	style.type = 'text/css';
-
-	// Define the CSS rules
-	var css = `
-		
-		li .Box__BoxComponent-sc-y4nds-0{
-			position: relative;
-			z-index: 10;
-			width: 100%;
-			height: 100%;
-		}
-		
-		li .Box__BoxComponent-sc-y4nds-0 .RowButton-sc-xxkq4e-0{
-			width: 100%;
-			height: 100%;
-			position: absolute;	
-			z-index: 20;
-		}
-
-		li .Box__BoxComponent-sc-y4nds-0 .Areas__HeaderArea-sc-8gfrea-3 .Areas__TrailingSlot-sc-8gfrea-7 button {
-			position: absolute;
-			left: 0px;
-			width: 100%;
-			height: 100%;
-			background-color: transparant;
-			opacity: 0.4;
-			z-index: 30;
-			border-left: 4px solid grey;
-			border-radius: 0px;
-			transform: scale(1) !important;
-		}
-		
-		button .IconWrapper__Wrapper-sc-16usrgb-0{
-			display: none;
-		}
-		
-		.main-yourLibraryX-collapseButton button span {
-			display: block;
-		}
-	`;
-
-	// Append the CSS rules to the <style> element
-	if (style.styleSheet) {
-		style.styleSheet.cssText = css; // IE8 and earlier
-	} else {
-		style.appendChild(document.createTextNode(css)); // Modern browsers
-	}
-
-	// Append the <style> element to the <head> of the document
-	document.head.appendChild(style);
-
-
 }
 
 export default main;
