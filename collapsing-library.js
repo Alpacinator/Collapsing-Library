74f4130bc06feb56f75baf70e573d7cc517fcd1b
Alpacinator <77163848+Alpacinator@users.noreply.github.com>
2025-02-12 14:16:00 +0100

    Update collapsing-library.js
    added new version

--- collapsing-library.js
+++ collapsing-library.js
@@ -1,66 +1,135 @@
 (async function() {
-        while (!Spicetify.React || !Spicetify.ReactDOM) {
-          await new Promise(resolve => setTimeout(resolve, 10));
-        }
-        var collapsingDlibrary = (() => {
-  // src/app.tsx
-  async function main() {
-    while (!(Spicetify == null ? void 0 : Spicetify.showNotification)) {
-      await new Promise((resolve) => setTimeout(resolve, 100));
-    }
-    var style = document.createElement("style");
-    style.type = "text/css";
-    var css = `
-		
-		li .Box__BoxComponent-sc-y4nds-0{
-			position: relative;
-			z-index: 10;
-			width: 100%;
-			height: 100%;
-		}
-		
-		li .Box__BoxComponent-sc-y4nds-0 .RowButton-sc-xxkq4e-0{
-			width: 100%;
-			height: 100%;
-			position: absolute;	
-			z-index: 20;
+	// Define color variables
+	const HOVER_FOLDER_INDICATOR_COLOR = 'yellow';
+	
+	const EXPAND_FOLDER_INDICATOR_COLOR = 'white';
+	const COLLAPSE_FOLDER_INDICATOR_COLOR = 'green';
+	
+	while (!Spicetify.React || !Spicetify.ReactDOM) {
+		await new Promise(resolve => setTimeout(resolve, 10));
+	}
+	var collapsingDlibrary = (() => {
+		// src/app.tsx
+		async function main() {
+			while (!(Spicetify == null ? void 0 : Spicetify.showNotification)) {
+				await new Promise((resolve) => setTimeout(resolve, 100));
+			}
+
+			// Add styles
+			var style = document.createElement("style");
+			style.type = "text/css";
+			var css = `
+				li .Box__BoxComponent-sc-y4nds-0{
+					position: relative;
+					z-index: 10;
+					width: 100%;
+					height: 100%;
+				}
+
+				li .Box__BoxComponent-sc-y4nds-0 .RowButton-sc-xxkq4e-0{
+					width: 100%;
+					height: 100%;
+					position: absolute;	
+					z-index: 20;
+				}
+
+				li .Box__BoxComponent-sc-y4nds-0 .Areas__HeaderArea-sc-8gfrea-3 .Areas__TrailingSlot-sc-8gfrea-7 button {
+					position: absolute;
+					left: 0px;
+					width: 100%;
+					height: 100%;
+					z-index: 30;
+					transform: scale(1) !important;
+					border: none !important;
+					opacity: 100 !important;
+				}
+
+				li .BRX6aJUAuAsvHKD_fpbo .HeaderArea .Areas__InteractiveArea-sc-8gfrea-0 button .IconWrapper__Wrapper-sc-16usrgb-0{
+					display: none;
+				}
+
+				button.collapse-button::before {
+					content: '';
+					position: absolute;
+					top: 50%;
+					left: 0px;
+					width: 5px;
+					opacity: 100 !important;
+					height: 50%;
+					background-color: ${COLLAPSE_FOLDER_INDICATOR_COLOR};
+					border-radius: 5px;
+					transform: translateY(-50%);
+				}
+
+				button.expand-button::before {
+					content: '';
+					position: absolute;
+					top: 50%;
+					left: 0px;
+					width: 5px;
+					opacity: 100%;
+					height: 50%;
+					background-color: ${EXPAND_FOLDER_INDICATOR_COLOR};
+					border-radius: 5px;
+					transform: translateY(-50%);
+				}
+			`;
+			if (style.styleSheet) {
+				style.styleSheet.cssText = css;
+			} else {
+				style.appendChild(document.createTextNode(css));
+			}
+			document.head.appendChild(style);
+
+			// Function to hide SVG icons in the library
+			const hideSVGIcons = () => {
+				const svgs = document.querySelectorAll('ul[aria-label="Your Library"] .Svg-img-icon-small');
+				svgs.forEach(svg => {
+					svg.style.display = 'none';  // Hide each SVG by setting display to 'none'
+				});
+			};
+
+			// Function to update button borders
+			const updateFolderIndicators = () => {
+				const collapseButtons = document.querySelectorAll('button[aria-label="Collapse folder"]');
+				const expandButtons = document.querySelectorAll('button[aria-label="Expand folder"]');
+				
+				// Add classes to buttons to target the ::before pseudo-element
+				collapseButtons.forEach(button => {
+					button.classList.add('collapse-button'); // Add class for collapse button
+				});
+
+				expandButtons.forEach(button => {
+					button.classList.add('expand-button'); // Add class for expand button
+				});
+			};
+
+			// Call hideSVGIcons and updateFolderIndicators when the page loads
+			hideSVGIcons();
+			updateFolderIndicators();
+
+			// Create a MutationObserver to watch for changes in the page and execute hideSVGIcons & updateFolderIndicators on each update
+			const observer = new MutationObserver((mutationsList) => {
+				mutationsList.forEach((mutation) => {
+					// Check if any added or removed nodes are relevant to our use case
+					if (mutation.type === 'childList') {
+						// Call hideSVGIcons and updateFolderIndicators on updates in the library
+						hideSVGIcons();
+						updateFolderIndicators();
+					}
+				});
+			});
+
+			// Observe changes in the body of the page (or a specific container if necessary)
+			const config = { childList: true, subtree: true };
+			observer.observe(document.body, config);
 		}
 
-		li .Box__BoxComponent-sc-y4nds-0 .Areas__HeaderArea-sc-8gfrea-3 .Areas__TrailingSlot-sc-8gfrea-7 button {
-			position: absolute;
-			left: 0px;
-			width: 100%;
-			height: 100%;
-			background-color: transparant;
-			opacity: 0.4;
-			z-index: 30;
-			border-left: 4px solid grey;
-			border-radius: 0px;
-			transform: scale(1) !important;
-		}
-		
-		li .BRX6aJUAuAsvHKD_fpbo .HeaderArea .Areas__InteractiveArea-sc-8gfrea-0 button .IconWrapper__Wrapper-sc-16usrgb-0{
-			display: none;
-		}
-		
-		.cVy6fGx6QtJbCnID7XdB>*, [dir=ltr] .cVy6fGx6QtJbCnID7XdB>:hover {
-			-webkit-transform: rotate(0deg); */
-			transform: rotate(0deg); */
-		}
-	`;
-    if (style.styleSheet) {
-      style.styleSheet.cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
-    }
-    document.head.appendChild(style);
-  }
-  var app_default = main;
+		var app_default = main;
 
-  // ../AppData/Local/Temp/spicetify-creator/index.jsx
-  (async () => {
-    await app_default();
-  })();
+		// Execute the function
+		(async () => {
+			await app_default();
+		})();
+	})();
 })();
-
-      })();
